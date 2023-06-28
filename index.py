d07a44a5b8363b34e3123a1f0d65355206340779
Jin Choi <jin.choi@sage.com>
2023-06-28 23:10:33 +0200

    GET to return template and refine POST

--- index.py
+++ index.py
@@ -26,27 +26,30 @@ def download_file(name):
     return send_from_directory(app.config["DOWNLOAD_FOLDER"], name, as_attachment=False)
 
 
-@app.route("/anonymise", methods=['POST'])
+@app.route("/", methods=['GET','POST'])
 def home():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        ofilename = 'anonymized_cv.txt'
-        ofilepath = os.path.join(app.config['DOWNLOAD_FOLDER'], ofilename)
-        with open(ofilepath, 'w', encoding='utf-8') as f:
-            f.write(anonymize(filepath))
-        return send_from_directory(app.config["DOWNLOAD_FOLDER"], ofilename, as_attachment=True)
-    else:
-        flash('Invalid file type')
-        return redirect(request.url)
+      if request.method == 'GET':
+        return render_template('index.html')
+      elif request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            ofilename = 'anonymized_cv.txt'
+            ofilepath = os.path.join(app.config['DOWNLOAD_FOLDER'], ofilename)
+            with open(ofilepath, 'w', encoding='utf-8') as f:
+                f.write(anonymize(filepath))
+            return send_from_directory(app.config["DOWNLOAD_FOLDER"], ofilename, as_attachment=True)
+        else:
+            flash('Invalid file type')
+            return redirect(request.url)
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")

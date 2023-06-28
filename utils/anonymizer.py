8f616cf89c39cc803845de9ae1e2e002c393f352
Jin Choi <jin.choi@sage.com>
2023-06-28 19:37:24 +0200

    feat: adjust transformers and torch and improve logics

--- utils/anonymizer.py
+++ utils/anonymizer.py
@@ -2,6 +2,8 @@ import pdfplumber
 import re
 import json
 import typing
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
 
 def extract_text(filepath: str) -> str:
@@ -12,6 +14,20 @@ def extract_text(filepath: str) -> str:
     for number, pageText in enumerate(pdf.pages):
         all_text += "\n" + pageText.extract_text()
     return all_text
+
+
+def first_filter(input_string):
+    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+    ner_results = nlp(input_string)
+    for result in ner_results:
+        start_index, end_index = result['start'], result['end']
+        mask = '*'
+        word_length = end_index - start_index
+        full_mask = mask * word_length
+        input_string = full_mask.join([input_string[:start_index], input_string[end_index:]])
+    return input_string
 
 
 def _get_personal_data(candidate_id: int) -> dict:
@@ -41,7 +57,10 @@ def final_filter(candidate_id: int, text: str) -> str:
         text = re.sub(escaped_keyword, "*****", text, flags=re.IGNORECASE)
     return text
 
+
 def anonymize(filename):
     text = extract_text(filename)
     mock_candidate_id = filename.split('.pdf')[-2][-1]  # Get mock candidate id from filename
-    return final_filter(candidate_id=mock_candidate_id, text=text)
+    filtered_text = first_filter(text)
+    output = final_filter(candidate_id=mock_candidate_id, text=filtered_text)
+    return output
